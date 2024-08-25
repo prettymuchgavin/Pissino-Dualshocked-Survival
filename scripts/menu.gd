ba9b037582e1f4b8d4de6b01460555278bd07d4a
cartoonist103 <tylergavin989@gmail.com>
2024-08-25 10:28:00 -0300

    removed multiplayer button temporarily

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -7,9 +7,6 @@ func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/intro.tscn")
 
 
-func _on_multiplayer_pressed():
-	Global.coop = true
-	get_tree().change_scene_to_file("res://scenes/intro.tscn")
 
 
 func _on_button_2_pressed():
